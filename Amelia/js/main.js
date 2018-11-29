30639f704560e785b91833d19d2910b7fba90894
ManonSgro <manon-sgro@outlook.fr>
2018-11-29 09:51:05 +0100

    first commit

---
+++ Amelia/js/main.js
@@ -0,0 +1,11 @@
+// dropdown
+/*var $ = jQuery.noConflict();
+	$(function() {
+		$('#activator').click(function(){
+			$('#box').animate({'top':'0px'},900);
+		});
+		$('#boxclose').click(function(){
+		$('#box').animate({'top':'-1000px'},900);
+		});
+	});	*/			
+
